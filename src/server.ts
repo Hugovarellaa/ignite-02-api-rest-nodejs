7d20a6f4de8c9e7bd43ccdc1b5df256ad8f63de5
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-15 15:40:47 -0300

    fix: Add and running eslint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/", () => {
-  return "Ola mundo!!";
-});
+app.get('/', () => {
+	return 'Ola mundo!!'
+})
 
 app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log("Server is running in port 3333");
-  });
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log('Server is running in port 3333')
+	})
