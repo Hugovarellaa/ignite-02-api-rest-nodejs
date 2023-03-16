f9df68f3b79cb44f95b4acda2e248a7f3562b9ad
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-15 23:12:15 -0300

    fix: Get Transaction with knex

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -5,6 +5,28 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+	app.get('/', async () => {
+		const transactions = await knex('transactions').select()
+
+		return {
+			transactions,
+		}
+	})
+
+	app.get('/:id', async (request, reply) => {
+		const getTransactionsParamsSchema = z.object({
+			id: z.string().uuid(),
+		})
+
+		const { id } = getTransactionsParamsSchema.parse(request.params)
+
+		const transactions = await knex('transactions').where('id', id).first()
+
+		return {
+			transactions,
+		}
+	})
+
 	app.post('/', async (request, reply) => {
 		const createTransactionBodySchema = z.object({
 			title: z.string(),
@@ -21,10 +43,5 @@ export async function transactionsRoutes(app: FastifyInstance) {
 		})
 
 		return reply.status(201).send()
-	})
-
-	app.get('/', async () => {
-		const transactions = await knex('transactions').select('*')
-		return transactions
 	})
 }
