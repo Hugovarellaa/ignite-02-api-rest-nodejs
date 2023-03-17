6fe29476d137ef82ff576bd9733c1b48071339f1
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-03-17 11:55:18 -0300

    fix: Config knex

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,12 @@
 import { Knex, knex as setupKnex } from 'knex'
 
-export const knex: Knex.Config = setupKnex({
+export const knexConfig: Knex.Config = {
 	client: 'sqlite',
 	connection: {
 		filename: './app.db',
 	},
 
 	useNullAsDefault: true,
-})
+}
+
+export const knex = setupKnex(knexConfig)
