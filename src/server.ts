4c62aa9ad307a9c8bde5e869b3cc9b22581c9194
hugo <hugovarellaa@gmail.com>
2023-04-13 17:46:37 -0300

    feat: create routes transactions

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
 import fastify from 'fastify';
-import { randomUUID } from 'node:crypto';
-import { knex } from './database';
 import { env } from './env';
+import { transactionsRoutes } from './routes/transaction.routes';
 
 const app = fastify();
-
-app.post('/', async () => {
-	const transactions = await knex('transactions')
-		.insert({
-			id: randomUUID(),
-			title: 'Transactions test',
-			amount: 1000,
-			created_at: new Date(),
-		})
-		.returning('*');
-
-	return transactions;
+app.register(transactionsRoutes, {
+	prefix: 'transactions',
 });
 
 app
