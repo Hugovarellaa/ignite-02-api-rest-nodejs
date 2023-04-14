ab59494cc931b43dfa821df47c961761a428ddd4
hugo <hugovarellaa@gmail.com>
2023-04-14 18:07:15 -0300

    feat: get transactions By Id

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -28,4 +28,16 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
 		return { transactions };
 	});
+
+	app.get('/:id', async (request) => {
+		const getTransactionParamsSchema = z.object({
+			id: z.string().uuid(),
+		});
+
+		const { id } = getTransactionParamsSchema.parse(request.params);
+
+		const transaction = await knex('transactions').where('id', id).first();
+
+		return { transaction };
+	});
 }
