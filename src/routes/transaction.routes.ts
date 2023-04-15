c9a5604b32843722c3e6d1cc1cd9b503b242d397
hugo <hugovarellaa@gmail.com>
2023-04-15 18:11:06 -0300

    feat: get summary transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -40,4 +40,10 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
 		return { transaction };
 	});
+
+	app.get('/summary', async () => {
+		const summary = await knex('transactions').sum('amount', { as: 'amount' }).first();
+
+		return { summary };
+	});
 }
