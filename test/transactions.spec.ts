f37e20acb307dbf2615d47c7f7ccaddedfe3f45b
hugo <hugovarellaa@gmail.com>
2023-04-19 20:05:18 -0300

    feat: it should be able to create a new transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,7 +1,23 @@
-import { describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { app } from '../src/app';
 
 describe('Transaction Routes', () => {
-	it('Primeiro test', () => {
-		expect(4 + 4).toEqual(8);
+	beforeAll(async () => {
+		await app.ready();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it('should be able to create a new transactions', async () => {
+		const response = await request(app.server).post('/transactions').send({
+			title: 'Transaction Test',
+			amount: 4000,
+			type: 'credit',
+		});
+
+		expect(response.statusCode).toBe(201);
 	});
 });
