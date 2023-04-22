5c8f6393cb6c62eb2b9f87106cf4f554792b74e7
hugo <hugovarellaa@gmail.com>
2023-04-22 20:40:59 -0300

    feat: create table in database test and exclude finished

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,5 +1,6 @@
+import { execSync } from 'node:child_process';
 import request from 'supertest';
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from 'vitest';
 import { app } from '../src/app';
 
 // Test E2E
@@ -11,6 +12,14 @@ describe('Transaction Routes', () => {
 
 	afterAll(async () => {
 		await app.close();
+	});
+
+	beforeEach(() => {
+		execSync('npm run knex -- migrate:latest');
+	});
+
+	afterEach(() => {
+		execSync('npm run knex -- migrate:rollback --all');
 	});
 
 	it('should be able to create a new transaction', async () => {
